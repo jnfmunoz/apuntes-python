dc54e0456aa8c99a3e3fe56fa63fc81c79f246e8
juan <juanmunozgrau@gmail.com>
2023-04-26 23:23:45 -0400

    cuarto commit+

--- Modulo-3/clase-ocho/index.py
+++ Modulo-3/clase-ocho/index.py
@@ -31,3 +31,50 @@ print(android.name)
 android.hidden_name = "Rojo"
 print(android.hidden_name)
 print(android.name)
+
+print("\n ******************************** \n")
+
+class CalculatedValue:
+    def __init__(self, _name, _height):
+        self.name = _name
+        self.height = _height
+
+    @property
+    def get_calculate_value(self) -> float:
+        return 0.35 * self.height
+    
+valuex= CalculatedValue("ratio", 10)
+
+print(f'El {valuex.name} es: {valuex.get_calculate_value}')
+
+print("\n ******************************** \n")
+
+class Dog:
+    obeys_owner = True
+
+    def __init__(self, _name):
+        self.name = _name
+    
+dog_one = Dog("Robin")
+dog_two = Dog("Malta")
+dog_three = Dog("Luz")
+
+print(f'{dog_one.name} obede a su dueño {dog_one.obeys_owner}')
+print(f'{dog_two.name} obede a su dueño {dog_two.obeys_owner}')
+print(f'{dog_three.name} obede a su dueño {dog_three.obeys_owner}')
+
+
+print("\n ******************************** \n")
+
+dog_one.obeys_owner = True
+#dog_two.obeys_owner = False
+print(f'{dog_one.name} obede a su dueño {dog_one.obeys_owner}')
+print(f'{dog_two.name} obede a su dueño {dog_two.obeys_owner}')
+print(f'{dog_three.name} obede a su dueño {dog_three.obeys_owner}')
+
+print("\n ******************************** \n")
+
+Dog.obeys_owner = False
+print(f'{dog_one.name} obede a su dueño {dog_one.obeys_owner}')
+print(f'{dog_two.name} obede a su dueño {dog_two.obeys_owner}')
+print(f'{dog_three.name} obede a su dueño {dog_three.obeys_owner}')
