9dfe950a98dae13ec671fc5f951cec6ac4843c66
juan <juanmunozgrau@gmail.com>
2023-05-03 00:12:31 -0400

    add-practice 7

--- Modulo-3/clase-12/transporte.py
+++ Modulo-3/clase-12/transporte.py
@@ -3,18 +3,17 @@ class Transporte:
         self.patentes = []
         self.movimientos = []
         self.tarifa = 730
-        self.tarifa = {
+        self.tarifario = {
             'adulto': self.tarifa,
             'niño':0,
             'adulto mayor': self.tarifa/2
         }
 
     def registrar_patente(self, _patente, _chofer):
-        patente = filter(lambda x: x['patente'] == _patente, self.patentes)
+        patente = list(filter(lambda x: x['patente'] == _patente, self.patentes))
         
-        if len(patente > 0):
-            print("La patente ya se encuentra registrada")
-            
+        if len(patente) > 0:
+            print("La patente ya se encuentra registrada")            
         else:
             self.patentes.append({
                 "patente": _patente,
@@ -24,8 +23,8 @@ class Transporte:
     def cobrar_pasaje(self, _patente, _tipo_persona, _fecha):
         self.movimientos.append({
             "patente": _patente,
-            "_tipo_persona": _tipo_persona,
-            "_fecha": _fecha,
+            "tipo_persona": _tipo_persona,
+            "fecha": _fecha,
             "monto_cobrado": self.tarifario[_tipo_persona],
         })
 
@@ -33,9 +32,20 @@ class Transporte:
         movimientos = list(filter(lambda x: x[_tipo_reporte]== _valor, self.movimientos))
         total_cobrado = 0
         for pasaje in movimientos:
-            total_cobrado += pasaje.monto
-            print(pasaje.fecha, pasaje.tipo_persona, pasaje.monto)
+            total_cobrado += pasaje['monto_cobrado']
+            print(pasaje['fecha'], pasaje['tipo_persona'], pasaje['monto_cobrado'])
+        
+        print("----------------")
+        print(f'total cobrado es: {total_cobrado}')
+        print("----------------")
 
 empresa = Transporte()
 
-empresa.registrar_patente("RR7266", "Rodrigo")
+empresa.registrar_patente("RR8266", "Rodrigo")
+
+empresa.cobrar_pasaje("RR8266", "adulto", "02-05-2023")
+empresa.cobrar_pasaje("RR8266", "adulto mayor", "02-05-2023")
+empresa.cobrar_pasaje("RR8266", "adulto", "02-05-2023")
+
+empresa.reporte_diario("tipo_persona", "adulto mayor")
+empresa.reporte_diario("tipo_persona", "adulto")
