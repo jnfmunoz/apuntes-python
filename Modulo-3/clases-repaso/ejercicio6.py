284ecdd09fb1fc0c23f8aaffa60b62329cfac0fd
juan <juanmunozgrau@gmail.com>
2023-05-07 18:05:52 -0400

    add-practice 11

--- Modulo-3/clases-repaso/ejercicio6.py
+++ Modulo-3/clases-repaso/ejercicio6.py
@@ -1,5 +1,12 @@
-n = int(input("Introduce la altura del triángulo (entero positivo): "))
-for i in range(n):
-    for j in range(i+1):
-        print("*", end="")
-    print("")
+def calculoPeso():
+    num_payasos = int(input('Ingrese cantidad de payasos vendidos: '))
+    num_munecas = int(input('Ingrese cantidad de muñecas vendidas: '))
+
+    peso_payaso = 112
+    peso_muneca = 75
+
+    peso_total = ((peso_payaso*num_payasos)+(peso_muneca*num_munecas))
+
+    print(f'El peso total del paquete a enviar es de: {peso_total} g')
+
+calculoPeso()
