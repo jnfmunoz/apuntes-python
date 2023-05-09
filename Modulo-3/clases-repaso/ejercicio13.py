df0485115765d6707eeeb342f7f33d14cab8c372
juan <juanmunozgrau@gmail.com>
2023-05-08 21:58:39 -0400

    add-practice 15

--- Modulo-3/clases-repaso/ejercicio13.py
+++ Modulo-3/clases-repaso/ejercicio13.py
@@ -1,14 +1,16 @@
 def tipoImpositivo():
     renta = int(input('Ingrese renta anual usuario: '))
     if renta < 10000:
-        print("Tienes que pagar el 5% de interés")
+        interes = "5%"
     elif renta >= 10000 and renta <= 20000:
-        print("Tienes que pagar el 15% de interés")
+        interes = "15%"
     elif renta > 20000 and renta <= 35000:
-        print("Tienes que pagar el 20% de interés")
+        interes = "20%"
     elif renta > 35000 and renta <= 60000:
-        print("Tienes que pagar el 30% de interés")
+        interes = "30%"
     elif renta > 60000:
-        print("Tienes que pagar el 45% de interés")
-        
+        interes = "45%"
+    
+    print(f"El tipo impositivo que corresponde es de {interes}")
+
 tipoImpositivo()
