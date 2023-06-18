48499f3c261ccecd52032800feb51d5eb4d8c9c4
Juan <juanmunozgrau@gmail.com>
2023-06-18 03:48:40 -0400

    add-practice 28

--- Modulo-4/practica-consolidacion/parte_3/clases.py
+++ Modulo-4/practica-consolidacion/parte_3/clases.py
@@ -1,12 +1,26 @@
 import csv
 
 class Vehiculo():
+
     def __init__(self, _marca, _modelo, _num_ruedas):
-        self.marca = _marca
-        self.modelo = _modelo
-        self.num_ruedas = _num_ruedas
-    
+
+        if es_cadena_no_vacia(_marca):
+            self.marca = _marca
+        else:
+            raise TypeError("marca de vehículo debe ser una cadena de caracteres no vacía")
+        
+        if es_cadena_no_vacia(_modelo):
+            self.modelo = _modelo
+        else:
+            raise TypeError("modelo de vehículo debe ser una cadena de caracteres no vacía")
+        
+        if es_numero(_num_ruedas):
+            self.num_ruedas = _num_ruedas
+        else:
+            raise TypeError("número de ruedas de vehículo debe ser un valor númerico")
+        
     def guardar(file_name, Vehiculo):
+        
         file = open(file_name, "a")
         datos = [(Vehiculo.__class__, Vehiculo.__dict__)]
         csv_file = csv.writer(file, lineterminator='\n')
@@ -15,6 +29,7 @@ class Vehiculo():
     
     
     def recuperar(file_name):
+        
         vehiculos = []
         file = open(file_name, "r")
         file_csv = csv.reader(file)
@@ -24,6 +39,7 @@ class Vehiculo():
         return vehiculos
     
     def imprimir_por_clase(vehiculos):
+        
         tipos = ["Particular", "Carga", "Bicicleta", "Motocicleta"]
 
         for tipo in tipos:
@@ -34,34 +50,54 @@ class Vehiculo():
                     print(vehiculo[1])    
 
     def __repr__(self) -> str:
+        
         """ Devuelve por defecto el nombre de la clase y la dirección del objeto. """
         return "Vehiculo"
 
 class Automovil(Vehiculo):
+
     def __init__(self, _marca, _modelo, _num_ruedas, _velocidad, _cilindrada):
-        self.marca = _marca
-        self.modelo = _modelo
-        self.num_ruedas = _num_ruedas
-        self.velocidad = _velocidad
-        self.cilindrada = _cilindrada
-    
+        
+        Vehiculo.__init__(self, _marca, _modelo, _num_ruedas)
+        
+        if es_numero(_velocidad):
+            self.velocidad = _velocidad
+        else:
+            raise TypeError("velocidad del vehículo debe ser un valor númerico")
+
+        if es_numero(_cilindrada):
+            self.cilindrada = _cilindrada
+        else:
+            raise TypeError("cilindrada del vehículo debe ser un valor númerico")
+        
     def __repr__(self) -> str:
         """ Devuelve por defecto el nombre de la clase y la dirección del objeto. """
         return "Automovil"
 
 class Particular(Automovil):
+
     def __init__(self, _marca, _modelo, _num_ruedas, _velocidad, _cilindrada, _numero_puestos):
         Automovil.__init__(self, _marca, _modelo, _num_ruedas, _velocidad, _cilindrada)
-        self.numero_puestos = _numero_puestos
+
+        if es_numero(_numero_puestos):
+            self.numero_puestos = _numero_puestos
+        else:
+            raise TypeError("número de puestos del vehículo debe ser un valor númerico")
     
     def __repr__(self) -> str:
         """ Devuelve por defecto el nombre de la clase y la dirección del objeto. """
         return "Particular"
     
 class Carga(Automovil):
+
     def __init__(self, _marca, _modelo, _num_ruedas, _velocidad, _cilindrada, _peso_carga):
         Automovil.__init__(self, _marca, _modelo, _num_ruedas, _velocidad, _cilindrada)
-        self.peso_carga = _peso_carga
+       
+        if es_numero(_peso_carga):
+            self.peso_carga = _peso_carga
+        else:
+            raise TypeError("número de puestos del vehículo debe ser un valor númerico") 
+        
     
     def __repr__(self) -> str:
         """ Devuelve por defecto el nombre de la clase y la dirección del objeto. """
@@ -70,7 +106,11 @@ class Carga(Automovil):
 class Bicicleta(Vehiculo):
     def __init__(self, _marca, _modelo, _num_ruedas, _tipo_bicicleta):
         Vehiculo.__init__(self, _marca, _modelo, _num_ruedas)
-        self.tipo_bicicleta = _tipo_bicicleta
+        
+        if es_cadena_no_vacia(_tipo_bicicleta):
+            self.tipo_bicicleta = _tipo_bicicleta
+        else:
+            raise TypeError("Tipo de bicicleta debe ser una cadena de caracteres no vacía")
     
     def __repr__(self) -> str:
         """ Devuelve por defecto el nombre de la clase y la dirección del objeto. """
@@ -79,10 +119,31 @@ class Bicicleta(Vehiculo):
 class Motocicleta(Bicicleta):
     def __init__(self, _marca, _modelo, _num_ruedas, _tipo_bicicleta, _motor, _cuadro, _nro_radios):
         Bicicleta.__init__(self, _marca, _modelo, _num_ruedas, _tipo_bicicleta)
-        self.motor = _motor
-        self.cuadro = _cuadro
-        self.nro_radios = _nro_radios
+        
+        if es_cadena_no_vacia(_motor):
+            self.motor = _motor
+        else:
+            raise TypeError("Motor de la motocicleta debe ser una cadena de caracteres no vacía")
+        
+        if es_cadena_no_vacia(_cuadro):
+            self.motor = _cuadro
+        else:
+            raise TypeError("Cuadro de motocicleta debe ser una cadena de caracteres no vacía")
+        
+        if es_numero(_nro_radios):
+            self.nro_radios = _nro_radios
+        else:
+            raise TypeError("Nro de radios debe ser un valor numérico")
     
     def __repr__(self) -> str:
         """ Devuelve por defecto el nombre de la clase y la dirección del objeto. """
         return "Motocicleta"
+
+def es_cadena_no_vacia(cadena):
+    """ Valida si una cadena de texto se encuentra vacía o no. """
+    if len(cadena) > 0:
+        return cadena
+
+def es_numero(valor):
+    """ Indica si un valor es númerico o no. """
+    return isinstance(valor, (int, float, complex))
--- Modulo-4/practica-consolidacion/parte_3/instancia-clase.py
+++ Modulo-4/practica-consolidacion/parte_3/instancia-clase.py
@@ -1,12 +1,12 @@
-from clases import Vehiculo, Particular, Carga, Bicicleta, Motocicleta
+from clases import Automovil, Vehiculo, Particular, Carga, Bicicleta, Motocicleta
 
-#automovil = Automovil("Ford", "Fiesta", "4", "180", "500")
-particular = Particular("Ford", "Fiesta", "4", "180", "500", "5")
-carga = Carga("Daft Trucks", "G 38", "10", "120", "1000", "20000")
+#automovil = Automovil("Ford", "Fiesta", 4, 180, 500)
+particular = Particular("Ford", "Fiesta", 4, 180, 500, 5)
+carga = Carga("Daft Trucks", "G 38", 10, 120, 1000, 20000)
 bicicleta = Bicicleta("Shimano", "MT Ranger", 2, "Carrera")
 motocicleta = Motocicleta("BMW", "F800s",2,"Deportiva","2T","Doble Viga", 21)
 
-#guardar("vehiculos.csv", automovil)
+#Vehiculo.guardar("vehiculos.csv", automovil)
 Vehiculo.guardar("vehiculos.csv", particular)
 Vehiculo.guardar("vehiculos.csv", carga)
 Vehiculo.guardar("vehiculos.csv", bicicleta)
