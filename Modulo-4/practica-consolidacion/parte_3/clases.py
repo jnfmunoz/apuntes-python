80aadd895706c235ef7a260f13835d436441bd8f
juan <juanmunozgrau@gmail.com>
2023-06-17 20:02:06 -0400

    add-practice 26

--- Modulo-4/practica-consolidacion/parte_3/clases.py
+++ Modulo-4/practica-consolidacion/parte_3/clases.py
@@ -1,9 +1,40 @@
+import csv
 class Vehiculo():
     def __init__(self, _marca, _modelo, _num_ruedas):
         self.marca = _marca
         self.modelo = _modelo
         self.num_ruedas = _num_ruedas
     
+    def guardar(file_name, Vehiculo):
+        file = open(file_name, "a")
+        datos = [(Vehiculo.__class__, Vehiculo.__dict__)]
+        csv_file = csv.writer(file)
+        csv_file.writerows(datos)
+        file.close()
+    
+    
+    def recuperar(file_name):
+        vehiculos = []
+        file = open(file_name, "r")
+        file_csv = csv.reader(file)
+        for vehiculo in file_csv:
+            vehiculos.append(vehiculo)
+        file.close()
+        return vehiculos
+    
+    def imprimir_por_clase(vehiculos):
+        tipos = ["Particular", "Carga", "Bicicleta", "Motocicleta"]
+
+        # for tipo in tipos:
+        #     print(f"\nLista de Vehículos {tipo}: ")
+        #     for vehiculo in vehiculos:
+        #         class_str = vehiculo[0].split("'")[1].rsplit('.', 1)[1]
+        #         if class_str == tipo:
+        #             print(vehiculo[1])
+        for x in tipos:
+            print(f"\nLista de Vehículos {x}: ")
+            
+
     def __repr__(self) -> str:
         """ Devuelve por defecto el nombre de la clase y la dirección del objeto. """
         return "Vehiculo"
@@ -58,26 +89,17 @@ class Motocicleta(Bicicleta):
         """ Devuelve por defecto el nombre de la clase y la dirección del objeto. """
         return "Motocicleta"
 
-#marca, modelo, num_ruedas, velocidad, cilindrada, num_puestos
-particular = Particular("Ford", "Fiesta", 4, "180", "500", 5)
-carga = Carga("Daft Trucks", "G 38", 10, 120, "1000", "20000")
+#automovil = Automovil("Ford", "Fiesta", "4", "180", "500")
+particular = Particular("Ford", "Fiesta", "4", "180", "500", "5")
+carga = Carga("Daft Trucks", "G 38", "10", "120", "1000", "20000")
 bicicleta = Bicicleta("Shimano", "MT Ranger", 2, "Carrera")
 motocicleta = Motocicleta("BMW", "F800s",2,"Deportiva","2T","Doble Viga", 21)
 
-print(f"Marca {particular.marca}, Modelo {particular.modelo}, {particular.num_ruedas} ruedas, {particular.velocidad} Km/h, {particular.cilindrada} cc, Puestos: {particular.numero_puestos}")
-print(f"Marca {carga.marca}, Modelo {carga.modelo}, {carga.num_ruedas} ruedas, {carga.velocidad} Km/h, {carga.cilindrada} cc, Carga: {carga.peso_carga} Kg")
-print(f"Marca {bicicleta.marca}, Modelo {bicicleta.modelo}, {bicicleta.num_ruedas} ruedas, Tipo: {bicicleta.tipo_bicicleta}")
-print(f"Marca {motocicleta.marca}, Modelo {motocicleta.modelo}, {motocicleta.num_ruedas} ruedas, Tipo: {motocicleta.tipo_bicicleta}, Motor: {motocicleta.motor}, Cuadro: {motocicleta.cuadro}, Nro Radios: {motocicleta.nro_radios}")
+#guardar("vehiculos.csv", automovil)
+Vehiculo.guardar("vehiculos.csv", particular)
+Vehiculo.guardar("vehiculos.csv", carga)
+Vehiculo.guardar("vehiculos.csv", bicicleta)
+Vehiculo.guardar("vehiculos.csv", motocicleta)
 
-def is_instance(valor, valor2):
-    if isinstance(valor, (valor2)):
-        print(f"{valor} es instancia con relación a {valor2.__name__}: {True}")
-    else:
-        print(f"{valor} es instancia con relación a {valor2.__name__}: {False}")
-
-is_instance(motocicleta, Vehiculo) #true
-is_instance(motocicleta, Automovil) #false
-is_instance(motocicleta, Particular) #false
-is_instance(motocicleta, Carga) #false
-is_instance(motocicleta, Bicicleta) #true
-is_instance(motocicleta, Motocicleta) #true
+vehiculos = Vehiculo.recuperar("vehiculos.csv")
+Vehiculo.imprimir_por_clase(vehiculos)
